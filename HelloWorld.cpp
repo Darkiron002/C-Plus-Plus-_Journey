4ed90e242522cc51ebbf89805cf4a98254b34a78
Darkiron002 <chayanmistry14@gmail.com>
2025-07-06 17:05:41 +0530

    First code for C++

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main (){
+    cout << "Hello world !"<< endl;
+    cout << "This is my first C++ program." << endl;
+    int x = 2, y = 4;
+    cout << x+y;
+    return 0;
+}
