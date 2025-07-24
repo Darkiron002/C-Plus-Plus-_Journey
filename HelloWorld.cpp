4855272958d250a3cc8487f51a194f290887c20f
Darkiron002 <chayanmistry14@gmail.com>
2025-07-24 18:37:19 +0530

    matrix operation

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
+
 int main(){
-    for(int i= 1; i <=5; i++){
-        for(int j= 1; j <= i; j++){
-            if(i % 2 != 0|| j %2 !=0){
-                cout<<"# "<< endl;
-            }
-            else{
-                cout << " ";
-            }
-        }cout<<" "<<endl;
-    } 
+    int matx[3][3];
+    for(int i = 0; i <3;i++){
+        for (int j =0; j <3 ; j++){
+            cin >> matx[i][j];
+        }
+    }
+    cout << "Matrix is: " << endl;
+    for(int i = 0; i <3;i++){
+        for (int j =0; j <3 ; j++){
+            cout << matx[i][j]<< " ";
+        }cout<< "\n";
+    }
+    
     return 0;
 }
