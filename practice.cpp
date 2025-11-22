02ad3c7c005409089ba1e29e57f5ad3156ac34a3
Darkiron002 <chayanmistry14@gmail.com>
2025-11-22 14:43:53 +0530

    Buble Sort

--- practice.cpp
+++ practice.cpp
@@ -1,52 +1,22 @@
-// ...existing code...
 #include <iostream>
 using namespace std;
-int FirstOcc(int arr[],int size,int key){
-    int start= 0;
-    int end= size -1;
-    int ans = -1;
-    int mid = start +(end-start)/2;
-    while(start <end){
-        if(arr[mid]==key){
-            ans = mid;
-            start = mid -1;
+int bublesort(int arr[],int n){
+    for(int i = 0; i<n;i++){
+        for(int j = 0;j<n;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
         }
-        else if(key >arr[mid]){
-            start = mid +1;
-        }
-        else if(key <arr[mid]){
-            end = mid -1;
-        }
-        
-        mid = start +(end-start)/2;
     }
-    return ans;
 }
-int LastOcc(int arr[],int size,int key){
-    int start= 0;
-    int end= size -1;
-    int ans = -1;
-    int mid = start +(end-start)/2;
-    while(start <end){
-        if(arr[mid]==key){
-            ans = mid;
-            start = mid +1;  
-        }
-        else if(key >arr[mid]){
-            start = mid +1;
-        }
-        else if(key <arr[mid]){
-            end = mid -1;
-        }
-        
-        mid = start +(end-start)/2;
+void printarr(int arr[],int n){
+    for(int i =0; i<n;i++){
+        cout<<arr[i]<<" ";
     }
-    return ans;
 }
 int main() {
-    int a[10]= {2,4,6,8,6,10,12};
-    cout <<"The index of the First occerence key is : "<<FirstOcc(a,6,6);
-    cout <<"\nThe index of the Last occerence key is : "<<LastOcc(a,6,6);
-    
+    int ar[5]= {5,8,9,10,3};
+    bublesort(ar,5);
+    printarr(ar,5);
     return 0;
 }
