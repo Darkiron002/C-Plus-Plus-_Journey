604bd712795f313aef8378389f00f526dc9cac96
Darkiron002 <chayanmistry14@gmail.com>
2025-11-26 00:08:01 +0530

    Printing of an array like a wave pattern

--- practice.cpp
+++ practice.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
+#include<vector>
 using namespace std;
-int bublesort(int arr[],int n){
-    for(int i = 0; i<n;i++){
-        for(int j = 0;j<n;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+//The function of operation 
+void wavecall(int arr[][3],int mrow, int mcol){
+    for(int col = 0;col <mcol;col++){
+        if(col%2!=0){
+            for(int row=mrow-1;row >=0;row--){
+                cout<<arr[row][col]<<" ";
+            }
+        }
+        else{
+            for(int row = 0; row< mrow;row++ ){
+                cout<<arr[row][col]<<" ";
             }
         }
     }
 }
-void printarr(int arr[],int n){
-    for(int i =0; i<n;i++){
-        cout<<arr[i]<<" ";
+//printing the array 
+void printarr(int arr[][3]){
+    cout<<"The matrix that you have entered is -"<<endl;
+    for(int i =0; i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
     }
 }
+//The main function 
 int main() {
-    int ar[5]= {5,8,9,10,3};
-    bublesort(ar,5);
-    printarr(ar,5);
+    int arr[3][3];
+    for(int i=0;i<3;i++){
+        for(int j = 0; j<3;j++){
+            cin>>arr[i][j];
+        }
+    }
+    wavecall(arr,3,3);
     return 0;
 }
